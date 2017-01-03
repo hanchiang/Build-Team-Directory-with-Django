7145f4fc9ddb9111fb0a2ff99a5e19041383a40b
han chiang <yaphc@hotmail.com>
2017-01-04 00:35:15 +0800

    deleted venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\han\desktop\mycodi~1\codesc~1\django\builda~1\build-~1\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
