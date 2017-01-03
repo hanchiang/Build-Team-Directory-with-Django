a203f80aa6c932597d0373e2ba19488002d3e274
han chiang <yaphc@hotmail.com>
2017-01-04 00:11:20 +0800

    Deploy working app

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\han\desktop\mycodi~1\codesc~1\django\builda~1\build-~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
